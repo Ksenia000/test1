2333fb21b629693ebae17a339e5e181fda8596e7
Oleksandr Kryvenko <okryven@softserveinc.com>
2019-09-25 11:13:14 +0300

    rename function and reformat code

--- Learn/js/string_script.js
+++ Learn/js/string_script.js
@@ -11,19 +11,20 @@ function rotateString(){
   alert('enough');
   }, 5000);
 }
-function Year(){
- var today = new Date();
- var year = today.getFullYear();
-//год, номер которого кратен 400, — високосный;
-//остальные годы, номер которых кратен 100, — невисокосные;
-//остальные годы, номер которых кратен 4, — високосные.
- var tmp400 = year%400;
- var tmp100 = year%100;
- var tmp4 = year%4;
-if (!tmp100 && tmp400 && tmp4) {
-  document.getElementById( "resYear" ).innerHTML = year + " is leap year in the Gregorian calendar )";
- } else {
-  document.getElementById( "resYear" ).innerHTML = year + " is'nt leap year in the Gregorian calendar :(";
-}
-console.log(year)
+
+function fillLeapYearElement() {
+    var today = new Date();
+    var year = today.getFullYear();
+    //год, номер которого кратен 400, — високосный;
+    var tmp400 = year % 400;
+    //остальные годы, номер которых кратен 100, — невисокосные;
+    var tmp100 = year % 100;
+    //остальные годы, номер которых кратен 4, — високосные.
+    var tmp4 = year % 4;
+    if (!tmp100 && tmp400 && tmp4) {
+        document.getElementById("resYear").innerHTML = year + " is leap year in the Gregorian calendar )";
+    } else {
+        document.getElementById("resYear").innerHTML = year + " is'nt leap year in the Gregorian calendar :(";
+    }
+    console.log(year)
 }
