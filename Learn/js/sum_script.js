775c33d54547c722c3fa265488666be0a75186b0
Your Name <you@example.com>
2019-10-17 22:15:11 +0300

    add git ignore file and reformat code

--- Learn/js/sum_script.js
+++ Learn/js/sum_script.js
@@ -1,19 +1,19 @@
-function fancSum (){
- var int1 = document.getElementById( "int1" ). value;
- var int2 = document.getElementById( "int2" ). value;
- var dataInt1 = parseInt( int1, 10 );
- var dataInt2 = parseInt( int2, 10 );
- var res;
- //console(int1,int2,dataInt1,dataInt2)
- if (int1 != dataInt1 || int1 != dataInt1) {
- document.getElementById( "sum" ).innerHTML ="*INCORRECT! Enter correct number!";
- } else {
-  if (int1 !== int2){
-  res = dataInt1 + dataInt2;
-  document. getElementById( "sum" ).innerHTML = res ;
-  } else {
-  res = dataInt1 * 3;
-  document. getElementById( "sum" ).innerHTML = res ;
-  }
- }
+function funcSum() {
+    var int1 = document.getElementById("int1").value;
+    var int2 = document.getElementById("int2").value;
+    var dataInt1 = parseInt(int1, 10);
+    var dataInt2 = parseInt(int2, 10);
+    var res;
+    //console(int1,int2,dataInt1,dataInt2)
+    if (int1 != dataInt1 || int2 != dataInt2) {
+        document.getElementById("sum").innerHTML = "*INCORRECT! Enter correct number!";
+    } else {
+        if (int1 !== int2) {
+            res = dataInt1 + dataInt2;
+            document.getElementById("sum").innerHTML = res;
+        } else {
+            res = dataInt1 * 3;
+            document.getElementById("sum").innerHTML = res;
+        }
+    }
 }
